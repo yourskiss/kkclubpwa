7fcb48cc10708ee0b3ad27993cbc84a2f813ad07
yourskiss <ksingh1817@gmail.com>
2024-05-05 16:57:37 +0530

    final

--- src/components/app/OfflineComponent.js
+++ src/components/app/OfflineComponent.js
@@ -1,6 +1,6 @@
 "use client";
 export default function OfflineComponent() {
-    const container =  { 'background-color':'#191919', 'width':'100%', 'height':'100vh', 'max-width':'1024px', 'margin':'0 auto', 'float':'none','display':'flex','align-items':'center', 'flex-direction':'column', 'justify-content':'center','padding':'1rem', 'box-sizing':'border-box' }
+    const container =  { 'position': 'absolute','left': '50%','top': '0','z-index':'9','transform':'translateX(-50%)', 'background-color':'#191919', 'width':'100%', 'height':'100vh', 'max-width':'1024px', 'margin':'0 auto', 'float':'none','display':'flex','align-items':'center', 'flex-direction':'column', 'justify-content':'center','padding':'1rem', 'box-sizing':'border-box' }
     const heading =  { 'width':'100%', 'margin':'0', 'float':'left','text-align':'center','color': '#fff','font-size': '24px','font-weight': 'bold','line-height': '1'  }
     const text =  { 'width':'100%', 'margin':'1rem 0 0 0', 'float':'left','text-align':'center','color': '#fff','font-size': '18px','font-weight': 'normal','line-height': '1'   }
     const holder =  { 'width':'100%', 'margin':'1.5rem 0 0 0', 'float':'left','text-align':'center' }
