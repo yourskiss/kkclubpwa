5a4cb5b7aea8fd4f23a27de1d46b99f31c74a34e
yourskiss <ksingh1817@gmail.com>
2024-05-10 14:44:16 +0530

    final

--- src/components/app/LoginComponent.js
+++ src/components/app/LoginComponent.js
@@ -170,28 +170,33 @@ export default function LoginComponent() {
     */
   }
 
-  if ('OTPCredential' in window) {
-    window.addEventListener('DOMContentLoaded', e => {
-      const input = document.querySelector('input[autocomplete="one-time-code"]');
-      if (!input) return;
-      const ac = new AbortController();
-      const form = input.closest('form');
-      if (form) {
-        form.addEventListener('submit', e => {
-          ac.abort();
+
+
+  useEffect(() => {
+    if ('OTPCredential' in window) {
+      window.addEventListener('DOMContentLoaded', e => {
+        const input = document.querySelector('input[autocomplete="one-time-code"]');
+        if (!input) return;
+        const ac = new AbortController();
+        const form = input.closest('form');
+        if (form) {
+          form.addEventListener('submit', e => {
+            ac.abort();
+          });
+        }
+        navigator.credentials.get({
+          otp: { transport:['sms'] },
+          signal: ac.signal
+        }).then(otp => {
+          input.value = otp.code;
+          if (form) form.submit();
+        }).catch(err => {
+          console.log(err);
         });
-      }
-      navigator.credentials.get({
-        otp: { transport:['sms'] },
-        signal: ac.signal
-      }).then(otp => {
-        input.value = otp.code;
-        if (form) form.submit();
-      }).catch(err => {
-        console.log(err);
       });
-    });
-  }
+    }
+  }, [isMobile]);
+
  
   return (
   <>
