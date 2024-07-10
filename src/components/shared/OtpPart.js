1fb5c22a58e5a87784e7b1ab81eef8917a1537a0
yourskiss <ksingh1817@gmail.com>
2024-07-10 22:09:14 +0530

    final

--- src/components/shared/OtpPart.js
+++ src/components/shared/OtpPart.js
@@ -36,7 +36,7 @@ export default function OtpPart({isMobStatus, getMobNumber, phonenumber}) {
     else{ 
       setOtpError('');
        console.log("otp part", phonenumber);
-      if(phonenumber === '9898989898' || phonenumber === '9876543210' || phonenumber === '9797979797' || phonenumber === '9090909090' || phonenumber === '9111111111' || phonenumber === '9191919191' || mobileValues === '9000000000')
+      if(phonenumber === '9898989898' || phonenumber === '9876543210' || phonenumber === '9797979797' || phonenumber === '9090909090' || phonenumber === '9111111111' || phonenumber === '9191919191' || phonenumber === '9000000000')
       {
         verifyDemo();
       }
